414504419d868f2d7f1a377500fd19e887a47b3e
Johan van Tent <tentvanjohan@hotmail.com>
2026-02-13 15:12:20 +0100

    Verbetering en correctie ivm nieuw meetpakket

--- R/CODE_bestrijdingsmiddelen.R
+++ R/CODE_bestrijdingsmiddelen.R
@@ -446,6 +446,39 @@ plot_overschr_freq <-
   theme(strip.text = element_text(size = 12, hjust = 0))
   # theme(panel.spacing = unit(25, "points"))
 
+plot_overschr_freq_excl_laag <-
+  data_gbm %>% 
+  filter(n() > 150 | jaar < 2024, .by = c(mp, datum)) %>% 
+  HHSKwkl::toetsing_gbm(normen, factor_detectiegrens = 0.5) %>%
+  filter(aantal > 1) %>% 
+  filter(jaar >= rap_jaar - 10) %>% 
+  mutate(landgebruik = str_to_sentence(f_landgebruik(mp))) %>%
+  mutate(landgebruik = case_when(
+    mp == "S_0504" ~ "Akkerbouw",
+    mp == "S_0609" ~ "Glastuinbouw", # S_0609 is lastig toe te delen - lijkt deels op glas
+    TRUE ~ landgebruik
+  )) %>%
+  group_by(jaar, landgebruik) %>%
+  summarise(n_toetsingen = n(),
+            n_overschrijdingen = sum(normoverschrijding),
+            fractie = n_overschrijdingen / n_toetsingen) %>%
+  mutate(landgebruik = fct_reorder(landgebruik, fractie, .desc = TRUE)) %>%
+  filter(!is.na(landgebruik), landgebruik != "Stedelijk") %>%
+  ggplot(aes(jaar, fractie)) +
+  geom_smooth(se = FALSE, method = "lm", linetype = "dashed", colour = blauw_l) +
+  geom_line(colour = blauw, linewidth = 1) +
+  geom_point(shape = 19, size = 2, colour = blauw) +
+  facet_wrap(~landgebruik, scales = "free", ncol = 2) +
+  scale_y_continuous(expand = c(0,0), limits = c(0, 0.035), labels = scales::percent_format(accuracy = 1)) +
+  scale_x_continuous(limits = c(rap_jaar - 10, rap_jaar), breaks = scales::breaks_pretty(6)) +
+  labs(title = "Hoe vaak wordt de norm overschreden?",
+       subtitle = "Uitgezonderd metingen met het nieuwe pakket",
+       x = "",
+       y = "% normoverschrijdingen") +
+  hhskthema() +
+  panel_theme_extra +
+  theme(strip.text = element_text(size = 12, hjust = 0))  
+
 # Niet toetsbare stoffen -----------------------------------------
 
 f_par_werking <-
