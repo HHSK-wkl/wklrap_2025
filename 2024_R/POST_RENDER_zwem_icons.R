0b8ddfe447be65ebd9e7c87162dd988fda1dd6fc
Johan van Tent <tentvanjohan@hotmail.com>
2026-02-16 13:04:26 +0100

    niet meer nodig

--- 2024_R/POST_RENDER_zwem_icons.R
+++
@@ -1,5 +0,0 @@
-# dir.create("_book/images")
-
-zwem_iconen_files <- list.files("images", pattern = "icon_zwemwater.*\\.png", full.names = TRUE)
-
-file.copy(zwem_iconen_files, file.path("_book/images", basename(zwem_iconen_files)))
